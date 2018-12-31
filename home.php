bf60eb1c7323c7a1b7f965562b0533e675766351
liyanageisu <36620128+liyanageisu@users.noreply.github.com>
2018-12-31 16:15:55 +0530

    Add files via upload
    It's a online job apply website.

---
+++ home.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+	
+	<title>Online Job Recruitment System</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+
+
+<body background="images/10.jpg">
+
+	
+	<div class="fixed1" style="opacity: 0.5">
+	<div class="header">
+	<img src="images/logo.png" align="right" width="180px">
+	<div class="t3">
+	Online Jobs
+	</div>
+	</div>
+	</div>
+	
+		<table border="0">
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+	
+	
+	
+	
+	<div class="fixed">		
+	<div class="leftpane"> 
+		<div class="about">
+			<div class="t1"><b> More </b></div><br>
+			<hr style="opacity: 0.2">
+			<div class="t2"><a href="../signup/Signup_form.php"> Contact us </a><br></div>
+			<div class="t2"><a href="../login/LOGin.php"> Help </a></div>
+		</div>
+	</div>
+	
+
+	<div class="rightpane">
+		<div class="about">
+			<div class="t1"> Keep In Touch </div><br>
+			<hr style="opacity: 0.2">
+			<a href="https://www.facebook.com/"><img src="images/facebooklogo.PNG" alt="facebook" class="logo"></a>
+			<a href="https://twitter.com/"><img src="images/tweeterlogo.PNG" alt="tweeter" class="logo"></a>
+			<a href="https://plus.google.com/discover"><img src="images/pluslogo.PNG" alt="googlePlus" class="logo"></a>
+			<a href="https://www.linkedin.com"><img src="images/linkedinlogo.PNG" alt="LinkedIn" class="logo"></a>
+			<a href="https://www.youtube.com/"><img src="images/youtubelogo.PNG" alt="youtube" class="logo"></a>
+			<a href="https://www.instagram.com/?hl=en"><img src="images/instalogo.PNG" alt="instagram" class="logo"></a>
+		</div>
+	</div>
+
+</div>
+</body>    	
+</html>
---
+++ jobapply.php
@@ -0,0 +1,99 @@
+<html>
+	<head>
+		<title>Job Apply</title>
+		<link rel="stylesheet" type="text/css" href="index.css">
+		<link rel="stylesheet" type="text/css" href="home.css">
+	</head>
+
+	<body background="images/10.jpg">
+
+	<div class="fixed1" style="opacity: 0.5">
+		<div class="header">
+			<img src="images/logo.png" align="right" width="180px">
+			<div class="t3">
+				Online Jobs
+			</div>
+		</div>
+	</div>
+		<div class="box">
+
+
+		<form>
+		
+			<font size="20"><center>Job Apply Form</center></font><br/>
+		
+		<table align="center" border="0">
+				
+			<tr>
+				<td width="100">First Name</td>	
+				<td><input type="text" name="first_name" placeholder="Enter First name" ></td>
+			</tr>
+			<tr>
+				<td>Middle Name</td>	
+				<td><input type="text" name="middle_name" placeholder="Enter Middle Name"></td>
+			</tr>
+			<tr>
+				<td>Last Name</td>	
+				<td><input type="text" name="last_name" placeholder="Enter Last Name"></td>
+			</tr>
+			<tr>
+				<td>Address</td>	
+				<td><input type="text" name="address" placeholder="Address" size="30"></td>
+			</tr>
+			<tr>
+				<td>Gender</td>	
+				<td><input type="radio" name="gender" value="male">Male
+					<input type="radio" name="gender" value="female">Female
+					</td>
+			</tr>
+			<tr>
+				<td>E-mail</td>	
+				<td><input type="text" name="email" placeholder="example@gmail.com" size="30"></td>
+			</tr>
+			<tr>
+				<td>Telephone</td>	
+				<td><input type="text" name="telephone" placeholder="+94xxxxxxxxxx"></td>
+			</tr>
+			<tr>
+				<td colspan="2" height="30">   </td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>
+				<input type="submit" value="submit">
+				<input type="reset" value="reset"></td>	
+			</tr>
+			
+		</table>
+		
+		</form>
+		</div>
+
+		<div class="fixed">		
+		<div class="leftpane"> 
+			<div class="about">
+				<div class="t1"><b> More </b></div><br>
+				<hr style="opacity: 0.2">
+				<div class="t2"><a href="../signup/Signup_form.php"> Contact us </a><br></div>
+				<div class="t2"><a href="../login/LOGin.php"> Help </a></div>
+			</div>
+		</div>
+
+		<div class="rightpane">
+			<div class="about">
+				<div class="t1"> Keep In Touch </div><br>
+				<hr style="opacity: 0.2">
+				<a href="https://www.facebook.com/"><img src="images/facebooklogo.PNG" alt="facebook" class="logo"></a>
+				<a href="https://twitter.com/"><img src="images/tweeterlogo.PNG" alt="tweeter" class="logo"></a>
+				<a href="https://plus.google.com/discover"><img src="images/pluslogo.PNG" alt="googlePlus" class="logo"></a>
+				<a href="https://www.linkedin.com"><img src="images/linkedinlogo.PNG" alt="LinkedIn" class="logo"></a>
+				<a href="https://www.youtube.com/"><img src="images/youtubelogo.PNG" alt="youtube" class="logo"></a>
+				<a href="https://www.instagram.com/?hl=en"><img src="images/instalogo.PNG" alt="instagram" class="logo"></a>
+			</div>
+		</div>
+
+	</div>
+
+
+	</body>
+</html>
